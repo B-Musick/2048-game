21885769c7f7a0895bada15c1858836752909a45
B-Musick <bmuze1@gmail.com>
2019-07-08 21:29:55 -0600

    board get method complete and prints board

--- board.js
+++ board.js
@@ -1,7 +1,13 @@
 class Board {
-    constructor(gridSize){
-        // Create 2D array of equal dimension (gridSize) and place 0 within
-        this.grid = [...Array(gridSize)].map(space => Array(gridSize).fill(0)); 
+    constructor(grid){
+        if(Number.isInteger(grid)){
+            // Create 2D array of equal dimension (gridSize) and place 0 within
+            this.grid = [...Array(gridSize)].map(space => Array(gridSize).fill(0));
+        }else{
+            // Otherwise its a predifined array
+            this.grid = grid;
+        }
+ 
         const UP = 'i';
         const DOWN = 'k';
         const LEFT = 'j';
@@ -39,21 +45,42 @@ class Board {
         console.log(arrCopy);
         return arr !== arrCopy;
     }
+
+    get board(){
+        // This will print the board
+        let grid = this.grid;
+        let board = ""; // This will hold the board to be printed
+        for(let i = 0; i<grid.length;i++){
+            for(let j = 0; j < grid.length;j++){
+                if(j!=grid.length-1){
+                    // If not at the end of the row
+                    grid[i][j] === 0 ? board += '-\t':board += `${grid[i][j]}\t`; 
+                }else{
+                    // If at the end of the row then need newline
+                    grid[i][j] === 0 ? board += '-\n' : board += `${grid[i][j]}\n`;
+                }
+            }
+        }
+        console.log(board);
+    }
 }
 
-// Test 1
-Board.changeLine([0, 2, 0, 0, 8, 0, 4]); // [ 2, 8, 4, 0, 0, 0, 0 ]
-console.log(Board.changeLine([0, 2, 0, 0, 8, 0, 4])); // true
+// // Test 1
+// Board.changeLine([0, 2, 0, 0, 8, 0, 4]); // [ 2, 8, 4, 0, 0, 0, 0 ]
+// console.log(Board.changeLine([0, 2, 0, 0, 8, 0, 4])); // true
 
-// Test 2 - Should combine adjacent values
-Board.changeLine([2, 0, 2, 4, 0, 0, 4]); // 2,0,4,2,0,0,4
-console.log(Board.changeLine([2, 0, 2, 4, 0, 0, 4])); // true
-Board.changeLine([2, 0, 4, 2, 0, 0, 4]); //[ 2, 4, 2, 4, 0, 0, 0 ]
-console.log(Board.changeLine([2, 0, 4, 2, 0, 0, 4])); // true
+// // Test 2 - Should combine adjacent values
+// Board.changeLine([2, 0, 2, 4, 0, 0, 4]); // 2,0,4,2,0,0,4
+// console.log(Board.changeLine([2, 0, 2, 4, 0, 0, 4])); // true
+// Board.changeLine([2, 0, 4, 2, 0, 0, 4]); //[ 2, 4, 2, 4, 0, 0, 0 ]
+// console.log(Board.changeLine([2, 0, 4, 2, 0, 0, 4])); // true
 
-// Test 3 - Shouldnt combine at same spot if already been combined
-Board.changeLine([2, 0, 2, 4, 0, 8]); //[ 4, 4, 8, 0, 0, 0 ]
+// // Test 3 - Shouldnt combine at same spot if already been combined
+// Board.changeLine([2, 0, 2, 4, 0, 8]); //[ 4, 4, 8, 0, 0, 0 ]
 
+// Test 4 - board should print
+let board = new Board([[2,3,0,0],[0,3,4,0],[5,0,5,0],[3,2,3,0]]);
+board.board;
 /********************THINGS LEARNED ************************************ */
 /* 
  * Create class method with keyword 'static' before function name 
