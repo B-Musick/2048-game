bd8d570b71c6c5fd140f3374f9fcdad8aba758ae
B-Musick <bmuze1@gmail.com>
2019-07-10 12:55:27 -0600

    extractLine method working

--- board.js
+++ board.js
@@ -7,7 +7,8 @@ class Board {
             // Otherwise its a predifined array
             this.grid = grid;
         }
- 
+        
+        // Used to control the board directions
         const UP = 'i';
         const DOWN = 'k';
         const LEFT = 'j';
@@ -15,35 +16,7 @@ class Board {
     }
     
     static changeLine(arr){
-        // // Shift single line according to game rule
-        // let arrCopy = [...arr] // Make deep copy of array
-        // let alreadyCombined = -1; // This will hold where the last adjacent values were defined
-        // // 1. Number tiles should shift left as far as possible
-        // for(let i = 0; i<arrCopy.length; i++){
-        //     if ((arrCopy[i] !== 0 && arrCopy[i - 1] === 0 )|| arrCopy[i] === arrCopy[i - 1]){
-        //         // If the current value is 0 and it can be moved left (since 0 before it)
-        //         // Or if there are no 0 before but the values next to eachother are the same
-        //         let count = i; // Store current i value
-        //         let movedVal = arrCopy[i]; // Store value to be moved
-        //         while(arrCopy[count-1]===0){
-        //             // While the value can be moved left (since 0)
-        //             count-=1;
-        //         }
-        //         if ((movedVal === arrCopy[count - 1]) && count-1 !== alreadyCombined){
-        //             // 2. If the value just moved matches the one beside it, combine them
-        //             // Cant combine them once a value has already been combined once this turn
-        //             arrCopy[count-1]+=movedVal;
-        //             alreadyCombined = count-1; 
-        //         }else{
-        //             // Place value moved at left most spot, if doesnt match the one beside it
-        //             arrCopy[count] = movedVal; 
-        //         }
-        //         arrCopy[i] = 0; // Set the spot where value moved from to 0
-
-        //     }
-        // }
-        // console.log(arrCopy);
-        // return arr !== arrCopy;
+        // Shift single line according to game rule
 
         // Remove all zeroes
         let noZeroArr = arr.filter((val) => val!==0);
@@ -66,25 +39,50 @@ class Board {
         }
         console.log(noZeroArr);
     }
-
-    get board(){
+    
+    // Public method to print the board
+    printBoard() {
         // This will print the board
-        let grid = this.grid;
+        let grid = [...this.grid];
         let board = ""; // This will hold the board to be printed
-        for(let i = 0; i<grid.length;i++){
-            for(let j = 0; j < grid.length;j++){
-                if(j!=grid.length-1){
-                    // If not at the end of the row
-                    grid[i][j] === 0 ? board += '-\t':board += `${grid[i][j]}\t`; 
-                }else{
-                    // If at the end of the row then need newline
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid.length; j++) {
+                j != grid.length - 1 ?
+                    // If not at the end of the row, then check if contains 0 or not
+                    grid[i][j] === 0 ? board += '-\t' : board += `${grid[i][j]}\t` :
+                    // If at the end of the row then need newline, then check if contains 0 or not
                     grid[i][j] === 0 ? board += '-\n' : board += `${grid[i][j]}\n`;
-                }
             }
         }
         console.log(board);
     }
+
+    extractLine(i, vertical, reverse){
+    // i is the column/row number
+    // Vertical is a boolean whether vertical line or not
+    // Reverse determines if line should be reversed
+    
+    // Return newly created array
+        let grid = this.grid;
+        let line = []; // This will hold the line to return
+
+        // Extract the line
+        // Verticle = true return column i, false return row i (grid[i])
+        !vertical ? line = grid[i] : grid.forEach((row) => {line.push(row[i])});
+
+        // Reverse the line if needed
+        // Reverse false return line normal, true then reverse it
+        reverse ? line = line.reverse() : line;
+
+        return line;
+    }
+
 }
+
+
+
+
+
 
 // // Test 1
 // Board.changeLine([0, 2, 0, 0, 8, 0, 4]); // [ 2, 8, 4, 0, 0, 0, 0 ]
@@ -100,9 +98,10 @@ class Board {
 // Board.changeLine([2, 0, 2, 4, 0, 8]); //[ 4, 4, 8, 0, 0, 0 ]
 
 // Test 4 - board should print
-// let board = new Board([[2,3,0,0],[0,3,4,0],[5,0,5,0],[3,2,3,0]]);
-// board.board;
+let board = new Board([[2,3,0,0],[0,3,4,0],[5,0,5,0],[3,2,3,0]]);
+board.printBoard();
 Board.changeLine([8, 8, 0, 0, 0, 0]);
+console.log(board.extractLine(2,true,true));
 /********************THINGS LEARNED ************************************ */
 /* 
  * Create class method with keyword 'static' before function name 
