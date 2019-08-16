9bae5bee4bb4d0189446b25bda87648c2686ddd7
B-Musick <bmuze1@gmail.com>
2019-08-16 16:26:39 -0600

    Board prints to browser, in blocks of equal size, printing value to screen

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Code inside said methods wholly my own and refactored for Javascript
 THINGS LEARNED
 - 'static' will make a class method
 - Normal methods made can be accessed through instances of the object
+- arr.splice(index, how many values) will return an array with the removed values (which is why in extractLine there is [0] at the end of the splice to only add the value removed not in an array). It will also leave the original array with the values removed.
+
+SVG
+- For adding text to say a rect, dont append it to the rect, append it to the actual display (see printBoard()
--- board.js
+++ board.js
@@ -7,16 +7,24 @@ class Board {
             // Otherwise its a predifined array
             this.grid = grid;
         }
-        
+        // Select the svg board
+        this.boardDisplay = d3.select('svg');
+
+        // Board Dimensions
+        this.blockWidth = (this.boardDisplay.attr('width'))/this.grid.length; // Width of individual blocks
+
         // Used to control the board directions
-        const UP = 'i';
-        const DOWN = 'k';
-        const LEFT = 'j';
-        const RIGHT = 'l';
+        this.UP = 'i';
+        this.DOWN = 'k';
+        this.LEFT = 'j';
+        this.RIGHT = 'l';
+
+        
     }
     
     static changeLine(arr){
         // Shift single line according to game rule
+        // 0 will represent empty space
 
         // Remove all zeroes
         let noZeroArr = arr.filter((val) => val!==0);
@@ -24,8 +32,10 @@ class Board {
         // Combine any adjacent values according to the rules
         let alreadyCombined = false;
         for(let i = 1; i< noZeroArr.length;i++){
+            // Start at 1 so dont check undefined values
             let currVal = i; // hold the current i value
             if ((noZeroArr[i] === noZeroArr[currVal - 1]) && currVal-1 !== alreadyCombined){
+                // If an value matches the one before it, and the index prior hasnt already been combined, then combine them
                 noZeroArr[currVal - 1] += parseInt(noZeroArr.splice(i,1)); // Splice value and add to adjacent
                 alreadyCombined = currVal-1; // Holds the combined value so dont combine again this turn
                 i--; // Since combined a value, backtrack
@@ -37,24 +47,104 @@ class Board {
             // Add zeroes to the rest of the array
             noZeroArr.push(0);
         }
-        console.log(noZeroArr);
+        // Return boolean whether array changed or not
+        return arr === noZeroArr; 
+        
     }
     
     // Public method to print the board
     printBoard() {
+        
+        console.log(this.blockWidth);
+
         // This will print the board
         let grid = [...this.grid];
         let board = ""; // This will hold the board to be printed
         for (let i = 0; i < grid.length; i++) {
             for (let j = 0; j < grid.length; j++) {
-                j != grid.length - 1 ?
+                if(j!= grid.length-1){
                     // If not at the end of the row, then check if contains 0 or not
-                    grid[i][j] === 0 ? board += '-\t' : board += `${grid[i][j]}\t` :
+
+                    if(grid[i][j] === 0){
+                        // If equals 0, then this represents - on the board 
+                        this.boardDisplay.append('rect')
+                            .attr('width', this.blockWidth)
+                            .attr('height', this.blockWidth)
+                            .attr('x', this.blockWidth*j)
+                            .attr('y',this.blockWidth*i)
+                            .style('fill','lightblue')
+
+                        this.boardDisplay.append('text')
+                            .text('-')
+                            .attr('x', this.blockWidth/2+(this.blockWidth * j))
+                            .attr('y', this.blockWidth / 2 +(this.blockWidth * i))
+                            .style('fill','black')
+                            .style('text-align', 'center')
+
+
+                    }else{
+                        this.boardDisplay.append('rect')
+                            .attr('width', this.blockWidth)
+                            .attr('height', this.blockWidth)
+                            .attr('x', this.blockWidth * j)
+                            .attr('y', this.blockWidth * i)
+                            .style('fill', 'lightgrey');
+
+                        this.boardDisplay.append('text')
+                                .text(grid[i][j]+"") 
+                                .attr('x', this.blockWidth / 2 + (this.blockWidth * j))
+                                .attr('y', this.blockWidth / 2 +(this.blockWidth * i))
+                                .style('fill', 'black')
+                                .style('text-align', 'center')
+                                .style('font-size','30')
+                    }
+                }else{
+
                     // If at the end of the row then need newline, then check if contains 0 or not
-                    grid[i][j] === 0 ? board += '-\n' : board += `${grid[i][j]}\n`;
+                    if (grid[i][j] === 0) {
+                        // If equals 0, then this represents - on the board 
+                        this.boardDisplay.append('rect')
+                            .attr('width', this.blockWidth)
+                            .attr('height', this.blockWidth)
+                            .attr('x', this.blockWidth / 2 + (this.blockWidth * j))
+                            .attr('y', this.blockWidth * i)
+                            .style('fill', 'lightblue')
+
+                        this.boardDisplay.append('text')
+                            .text('-')
+                            .attr('x', this.blockWidth / 2 + (this.blockWidth * j))
+                            .attr('y', this.blockWidth / 2 + (this.blockWidth * i))
+                            .style('fill', 'black')
+                            .style('text-align', 'center')
+
+
+                    } else {
+                        // If not 0 then put the value
+
+                        // Add blocks to screen
+                        this.boardDisplay.append('rect')
+                            .attr('width', this.blockWidth)
+                            .attr('height', this.blockWidth)
+                            .attr('x', this.blockWidth * j)
+                            .attr('y', this.blockWidth * i)
+                            .style('fill', 'lightgrey');
+                        
+                        // Add block values to screen
+                        this.boardDisplay.append('text')
+                            .text(grid[i][j] + "")
+                            .attr('x', this.blockWidth / 2 + (this.blockWidth * j))
+                            .attr('y', this.blockWidth / 2 + (this.blockWidth * i))
+                            .style('fill', 'black')
+                            .style('text-align', 'center')
+                            .style('font-size', '30')
+                    }
+                }
+                    
             }
         }
-        console.log(board);
+        
+        
+        
     }
 
     extractLine(i, vertical, reverse){
@@ -93,6 +183,7 @@ class Board {
             
             
         }else{
+            // Can just insert the line array since rows are [[row],[row]]
             this.grid.splice(i,0,line);
         }
 
@@ -102,7 +193,10 @@ class Board {
         // This should return a new board object in the shifted form in direction
         // The existing board shouldnt be
         let thisBoard = new Board([...this.grid]); // Make deep copy of board
-        
+        console.log(thisBoard.printBoard());
+        if(direction===this.LEFT||direction===this.RIGHT){
+            console.log('fuck');
+        }
 
 
     }
@@ -129,6 +223,7 @@ class Board {
 
 // Test 4 - board should print
 let board = new Board([[1,2,3],[4,5,6],[7,8,9]]);
+
 board.printBoard();
 Board.changeLine([8, 8, 0, 0, 0, 0]);
 board.extractLine(2,true,true);
